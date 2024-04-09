419eb6aa90464a92a3927efcabaea022bce2004b
Sanusan-Ponnuthurai <iamshanusan@gmail.com>
2024-04-09 09:46:21 +0530

    changed

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -2,36 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const users = [{name:"Pedro",age:21},{name:"Jake",age:25},{name:"Jessica",age:31}]
   return (
     <div>
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <ConRender />
+      {users.map((user,key) => {
+        return <User name ={user.name} age={user.age} />
+      })}
     </div>
   );
+
 };
 
-const Job = (props) => {
+
+const User = (props) => {
   return (
     <div>
-    <h1>{props.salary}</h1>
-    <h1>{props.position}</h1>
-    <h1>{props.company}</h1>
+      {props.name} {props.age}
     </div>
-  );
-};
-
-const ConRender = () => {
-  const age = 15;
-  const isGreen =true;
-  const isWhite =false;
-  return (
-    <div>
-      {age >=18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
-      {isGreen && <button>Button</button>}
-      <h1 style = {{color:isWhite ? "yellow" : "red"}}>This text is yellow</h1>
-    </div>
-  );
-};
-
+  )
+}  
+    
 export default App;
